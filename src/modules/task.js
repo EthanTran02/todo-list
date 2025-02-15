22c2fc15fc0560297902afe76403fc6555699866
Ethan <khoitranec@gmail.com>
2025-02-15 21:59:13 +0700

    Updates

--- src/modules/task.js
+++
@@ -1,9 +0,0 @@
-export default class Task {
-    constructor(title, description, dueDate, priority) {
-        this.title = title
-        this. description = description
-        this.dueDate = dueDate
-        this.priority = priority
-        this.complete = false
-    }
-}
